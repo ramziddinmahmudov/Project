f6f861cdd600748896d5c8751d9264af835a1a3c
Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-10 05:25:36 +0500

    AS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad287ca0f5f100db"; }
+/******/ 		__webpack_require__.h = function() { return "c9377db1407a9e4f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -12,9 +12,9 @@ const Contact = () => {
       <div data-aos="fade-up" className="contact-text">
         <h2 className="mezzardBold">SAVOLLAR BO’LSA MENEDJERGA MUROJAAT QILISHINGIZ MUMKIN</h2>
         <div className="telegram-contact">
-          <a href="https://t.me/arashov_trader" target="_blank">
+          <a href="https://t.me/ARASHOV_MANAGER" target="_blank">
             <Image src={TelegramIcon} alt="TelegramIcon" width={37} />
-            <p className="mezzardBold">@ARASHOV_TRADER</p>
+            <p className="mezzardBold">@ARASHOV MANEGER</p>
           </a>          
         </div>
         <p className='phone-number mezzardBold'><a href="tel:+998955051199">+998 (95) 505-11-99</a></p>
